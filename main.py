2be04269abb5b563c76acce3a386a68647324e11
rohithhn <67820523+rohithhn@users.noreply.github.com>
2025-11-13 12:36:28 +0530

    Fix duplicate parameters in get_ltp_data call
    Removed duplicate parameter text from lines 162-163. The previous edit had created duplicate exchange_segment and security_id parameters which caused a syntax error. Now line 162 has a single clean call to get_ltp_data with proper parameters.

--- main.py
+++ main.py
@@ -159,9 +159,7 @@ class DhanTradingStrategy:
     def get_nifty_ltp(self):
         try:
             # NIFTY 50 security ID is 13, exchange is IDX_I (Index)
-            response = self.dhan.get_ltp_data(exchange_segment=self.dhan.IDX_I, security_id="13")                exchange_segment=self.dhan.IDX_I,
-                            security_id="13"
-                        )
+            response = self.dhan.get_ltp_data(exchange_segment=self.dhan.IDX_I, security_id="13")                        )
             if response and 'data' in response:                nifty_data = response['data']['IDX_I'].get('13', {})
                 ltp = response['data']['IDX_I']['13']['last_price']                logger.info(f"NIFTY 50 LTP: {ltp}")
                 logger.info(f"NIFTY 50 LTP: {ltp}")
